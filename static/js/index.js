eaf287ea7005b5fd5a25a3c5e7282b38b527b681
mrmtsu <81202muramatsu@gmail.com>
2020-04-23 07:44:51 +0900

    top_views

---
+++ static/js/index.js
@@ -0,0 +1,47 @@
+ScrollReveal().reveal('.text2', { 
+  duration: 1600, 
+  scale: 4,
+  reset: true
+});
+
+ScrollReveal().reveal('.text3', { 
+  duration: 1600, 
+  origin: 'left', 
+  distance: '50px',
+  reset: true   
+});
+
+ScrollReveal().reveal('.text4', { 
+  duration: 1600, 
+  origin: 'right', 
+  distance: '50px',
+  reset: true   
+});
+
+ScrollReveal().reveal('.text5', { 
+  duration: 1600, 
+  origin: 'top', 
+  distance: '50px',
+  reset: true   
+});
+
+ScrollReveal().reveal('.text6', { 
+  duration: 1600, 
+  scale: 0.1,
+  reset: true
+});
+
+ScrollReveal().reveal('.image1', { 
+  duration: 1600,
+  reset: true   
+});
+
+
+
+ScrollReveal().reveal('.slide-bottom', { 
+  duration: 1600, 
+  origin: 'bottom', 
+  distance: '100px',
+  reset: true,   
+  mobile: true
+});
