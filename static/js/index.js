71202bb20a0b9f6e3f07d512a3fba65d9254dfb5
mrmtsu <81202muramatsu@gmail.com>
2020-04-23 11:06:36 +0900

    view

--- static/js/index.js
+++ static/js/index.js
@@ -45,3 +45,49 @@ ScrollReveal().reveal('.slide-bottom', {
   reset: true,   
   mobile: true
 });
+
+
+ScrollReveal().reveal('.first', { 
+  duration: 1600, 
+  origin: 'left', 
+  distance: '100px',
+  reset: true,   
+  mobile: true
+});
+
+ScrollReveal().reveal('.second', { 
+  duration: 1600, 
+  origin: 'right', 
+  distance: '100px',
+  reset: true,   
+  mobile: true
+});
+
+ScrollReveal().reveal('#blog', { 
+  duration: 3000,
+  reset: true   
+});
+
+
+// $(function(){
+//   $(`#blind_menu`).hide();
+//   $('.menu-trigger.active').hide();
+//   $('#hamburger').click(function(e){
+//     $('#blind_menu').show();
+//     $('.menu-trigger').hide();
+//     $('.menu-trigger.active').show();
+//     $('#hamburger').click(function(e){
+//       $('#blind_menu').hide();
+//       $('.menu-trigger').show();
+//       $('.menu-trigger.active').hide();
+//     })
+//   })
+// })
+
+
+$(function(){
+  $('#hamburger').click(function(e){
+    $('.menu-trigger').toggleClass('active');
+    $('#blind_menu').toggleClass('open_menu');
+  });
+});
