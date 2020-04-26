5e7ddeec54b89ce1e53bf71f2bf274fbfddd607c
mrmtsu <81202muramatsu@gmail.com>
2020-04-27 07:27:09 +0900

    s3_upload

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -153,7 +153,7 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     MEDIA_URL = S3_URL
